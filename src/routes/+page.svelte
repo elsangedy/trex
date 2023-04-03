d74f165e0b53a9936177b32417de68b2922eafce
Munir Ahmed Elsangedy <elsangedy@gmail.com>
2023-04-02 21:51:31 -0300

    Add new exercises and move to cdn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,8 @@
 	import Exercice from '$lib/components/exercice.svelte';
 
 	const dayOfWeek = new Date().getDay();
+
+	const IMAGE_URL_PREFIX = 'https://ik.imagekit.io/29ddu02vk/trex';
 </script>
 
 <Accordion.default>
@@ -13,34 +15,50 @@
 		<Accordion.Header>Segunda-feira</Accordion.Header>
 		<Accordion.Content class="accordion-content">
 			<List.default twoLine>
-				<Exercice name="Warmup / Core" media="/media/warmup.png" kind="warmup" />
+				<Exercice name="Warmup / Core" media={`${IMAGE_URL_PREFIX}/warmup.png`} kind="warmup" />
 				<Separator.default />
 				<Exercice name="Clean Complex" kind="skill" />
 				<Separator.default />
 				<Exercice
-					name="Hang Power Clean"
+					name="Clean Pull"
+					sets={5}
+					reps={3}
+					kind="power"
+					media={`${IMAGE_URL_PREFIX}/clean-pull.gif`}
+				/>
+				<Separator.default />
+				<Exercice
+					name="Back Squat"
 					sets={4}
 					reps={5}
 					kind="power"
-					media="/media/hang-power-clean.gif"
+					media={`${IMAGE_URL_PREFIX}/back-squat.gif`}
 				/>
-				<Separator.default />
-				<Exercice name="Back Squat" sets={4} reps={5} kind="power" media="/media/back-squat.gif" />
 				<Separator.default />
 				<Exercice
 					name="1 Arm DB Bench Press"
 					sets={3}
 					reps="8 + 8"
-					media="/media/1-arm-db-bench-press.gif"
+					media={`${IMAGE_URL_PREFIX}/1-arm-db-bench-press.gif`}
 				/>
-				<Exercice name="Lateral Step Up" sets={3} reps="8 + 8" media="/media/lateral-step-up.gif" />
-				<Separator.default />
-				<Exercice name="1 DB 1 Leg RDL" sets={3} reps="8 + 8" media="/media/1-db-1-leg-rdl.gif" />
 				<Exercice
-					name="Kneeling Low To High Woodchop"
+					name="Lateral Lunges"
 					sets={3}
-					reps="12 + 12"
-					media="/media/kneeling-low-to-high-woodchop.gif"
+					reps="8 + 8"
+					media={`${IMAGE_URL_PREFIX}/lateral-lunges.gif`}
+				/>
+				<Separator.default />
+				<Exercice
+					name="2 DB 1 Leg RDL"
+					sets={3}
+					reps="8 + 8"
+					media={`${IMAGE_URL_PREFIX}/2-db-1-leg-rdl.gif`}
+				/>
+				<Exercice
+					name="Band Spiderman Twist"
+					sets={3}
+					reps="8 + 8"
+					media={`${IMAGE_URL_PREFIX}/band-spiderman-twist.gif`}
 				/>
 				<Separator.default />
 				<Exercice name="Cool Down" />
@@ -51,22 +69,49 @@
 		<Accordion.Header>Terça-feira</Accordion.Header>
 		<Accordion.Content class="accordion-content">
 			<List.default twoLine>
-				<Exercice name="Warmup / Core" media="/media/warmup.png" kind="warmup" />
+				<Exercice name="Warmup / Core" media={`${IMAGE_URL_PREFIX}/warmup.png`} kind="warmup" />
 				<Separator.default />
-				<Exercice name="Deadlift" sets={4} reps={5} kind="power" media="/media/deadlift-side.gif" />
+				<Exercice
+					name="Deadlift"
+					sets={4}
+					reps={5}
+					kind="power"
+					media={`${IMAGE_URL_PREFIX}/deadlift-side.gif`}
+				/>
 				<Separator.default />
-				<Exercice name="Push Press" sets={4} reps={5} kind="power" media="/media/push-press.gif" />
+				<Exercice
+					name="Push Press"
+					sets={4}
+					reps={5}
+					kind="power"
+					media={`${IMAGE_URL_PREFIX}/push-press.gif`}
+				/>
 				<Separator.default />
-				<Exercice name="1 Arm DB Row" sets={3} reps="8 + 8" media="/media/1-arm-db-row.gif" />
-				<Exercice name="BB Split Squat" sets={3} reps="8 + 8" media="/media/bb-split-squat.gif" />
+				<Exercice
+					name="1 Arm DB Row"
+					sets={3}
+					reps="8 + 8"
+					media={`${IMAGE_URL_PREFIX}/1-arm-db-row.gif`}
+				/>
+				<Exercice
+					name="BB Backward Lunges"
+					sets={3}
+					reps="8 + 8"
+					media={`${IMAGE_URL_PREFIX}/bb-backward-lunges.gif`}
+				/>
 				<Separator.default />
 				<Exercice
 					name="Shoulder Complex"
 					sets={3}
 					reps="8 + 8 + 8"
-					media="/media/shoulder-complex-front.gif"
+					media={`${IMAGE_URL_PREFIX}/shoulder-complex-front.gif`}
 				/>
-				<Exercice name="AB Wheel Rollout" sets={3} reps={12} media="/media/ab-wheel-rollout.gif" />
+				<Exercice
+					name="AB Wheel Rollout"
+					sets={3}
+					reps={12}
+					media={`${IMAGE_URL_PREFIX}/ab-wheel-rollout.gif`}
+				/>
 				<Separator.default />
 				<Exercice name="Cool Down" />
 			</List.default>
@@ -76,16 +121,16 @@
 		<Accordion.Header>Quinta-feira</Accordion.Header>
 		<Accordion.Content class="accordion-content">
 			<List.default twoLine>
-				<Exercice name="Warmup / Core" media="/media/warmup.png" kind="warmup" />
+				<Exercice name="Warmup / Core" media={`${IMAGE_URL_PREFIX}/warmup.png`} kind="warmup" />
 				<Separator.default />
 				<Exercice name="Clean Complex" kind="skill" />
 				<Separator.default />
 				<Exercice
-					name="Hang Squat Clean"
-					sets={4}
-					reps={5}
+					name="Clean"
+					sets={5}
+					reps={3}
 					kind="power"
-					media="/media/hang-squat-clean.gif"
+					media={`${IMAGE_URL_PREFIX}/hang-squat-clean.gif`}
 				/>
 				<Separator.default />
 				<Exercice
@@ -93,24 +138,34 @@
 					sets={4}
 					reps={5}
 					kind="power"
-					media="/media/bench-press.gif"
+					media={`${IMAGE_URL_PREFIX}/bench-press.gif`}
 				/>
 				<Separator.default />
-				<Exercice name="Inverted Row" sets={3} reps={12} media="/media/inverted-row.gif" />
+				<Exercice
+					name="Inverted Row"
+					sets={3}
+					reps={12}
+					media={`${IMAGE_URL_PREFIX}/inverted-row.gif`}
+				/>
 				<Exercice
 					name="DB Bulgarian Squat"
 					sets={3}
 					reps="8 + 8"
-					media="/media/db-bulgarian-squat.gif"
+					media={`${IMAGE_URL_PREFIX}/db-bulgarian-squat.gif`}
 				/>
 				<Separator.default />
 				<Exercice
-					name="Banded Good Morning"
+					name="BB Good Morning"
 					sets={3}
 					reps={12}
-					media="/media/banded-good-morning.gif"
+					media={`${IMAGE_URL_PREFIX}/bb-good-morning.gif`}
 				/>
-				<Exercice name="Wall Ball Throw" sets={3} reps="8 + 8" media="/media/wall-ball-throw.gif" />
+				<Exercice
+					name="Lateral Wall Ball Throw"
+					sets={3}
+					reps="8 + 8"
+					media={`${IMAGE_URL_PREFIX}/lateral-wall-ball-throw.gif`}
+				/>
 				<Separator.default />
 				<Exercice name="Cool Down" />
 			</List.default>
@@ -120,7 +175,7 @@
 		<Accordion.Header>Sexta-feira</Accordion.Header>
 		<Accordion.Content class="accordion-content">
 			<List.default twoLine>
-				<Exercice name="Warmup / Core" media="/media/warmup.png" kind="warmup" />
+				<Exercice name="Warmup / Core" media={`${IMAGE_URL_PREFIX}/warmup.png`} kind="warmup" />
 				<Separator.default />
 				<Exercice name="Tempo Squat" kind="skill" />
 				<Separator.default />
@@ -129,29 +184,50 @@
 					sets={4}
 					reps={5}
 					kind="power"
-					media="/media/front-squat.gif"
+					media={`${IMAGE_URL_PREFIX}/front-squat.gif`}
 				/>
 				<Separator.default />
-				<Exercice name="Pull Ups" sets={4} reps="MAX" kind="power" media="/media/pull-ups.gif" />
+				<Exercice
+					name="Pull Ups"
+					sets={4}
+					reps="MAX"
+					kind="power"
+					media={`${IMAGE_URL_PREFIX}/pull-ups.gif`}
+				/>
 				<Separator.default />
 				<Exercice
 					name="Kneeling 1 Arm DB Press"
 					sets={3}
 					reps="8 + 8"
-					media="/media/kneeling-1-arm-db-press.gif"
+					media={`${IMAGE_URL_PREFIX}/kneeling-1-arm-db-press.gif`}
 				/>
-				<Exercice name="KB Swings" sets={3} reps={12} media="/media/kb-swings.gif" />
+				<Exercice name="KB Swings" sets={3} reps={12} media={`${IMAGE_URL_PREFIX}/kb-swings.gif`} />
 				<Separator.default />
 				<Exercice
-					name="1 Arm Overhead Carry"
+					name="1 Side Suitcase Carry"
 					sets={3}
 					reps="20 + 20"
-					media="/media/1-arm-overhead-carry.gif"
+					media={`${IMAGE_URL_PREFIX}/1-side-suitcase-carry.gif`}
 				/>
-				<Exercice name="Banded Punch" sets={3} reps="8 + 8" media="/media/banded-punch.gif" />
+				<Exercice
+					name="Banded Twist"
+					sets={3}
+					reps="8 + 8"
+					media={`${IMAGE_URL_PREFIX}/banded-twist.gif`}
+				/>
 				<Separator.default />
-				<Exercice name="Bicep Curls" sets={3} reps="10 + 10" media="/media/bicep-curls.gif" />
-				<Exercice name="Skull Crushers" sets={3} reps="10 + 10" media="/media/skull-crushers.gif" />
+				<Exercice
+					name="Bicep Curls"
+					sets={3}
+					reps="10 + 10"
+					media={`${IMAGE_URL_PREFIX}/bicep-curls.gif`}
+				/>
+				<Exercice
+					name="Skull Crushers"
+					sets={3}
+					reps="10 + 10"
+					media={`${IMAGE_URL_PREFIX}/skull-crushers.gif`}
+				/>
 				<Separator.default />
 				<Exercice name="Cool Down" />
 			</List.default>
